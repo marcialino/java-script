600e4b349d51da18617c053126e01d97652690cb
marcialino <marcialino100@gmail.com>
2023-03-10 09:03:04 -0300

    atualizei ex015

--- Aula012exe/ex015/script.js
+++ Aula012exe/ex015/script.js
@@ -9,12 +9,40 @@ function verificar(){
        var fsex = document.getElementsByName('radsex')
        var idade = ano - Number(fano.value)
        var genero = ''
+       var img = document.createElement('img')
+       img.setAttribute('id', 'foto')
        if (fsex[0].checked){
             genero = 'Homem'
+            if(idade >=0 && idade <10){
+               //criança
+               img.setAttribute('src', 'menino.png')
+            }else if(idade < 21){
+               //jovem
+               img.setAttribute('src', 'moco.png')
+            }else if (idade > 21 && idade < 60){
+               //adulto
+               img.setAttribute('src', 'adulto.png')
+            }else{
+               //idoso
+               img.setAttribute('src', 'idoso.png')
+            }
        }else if (fsex[1].checked){
             genero = 'Mulher'
-       }
+          }if (idade >=0 && idade < 10){
+          //criança
+          img.setAttribute('src', 'menina.png')
+           }else if (idade < 21){
+          //jovem
+          img.setAttribute('src', 'moca.png')
+           }else if (idade > 21 && idade < 60){
+          //adulto
+          img.setAttribute('src', 'adulta.png')
+           }else{
+          //idoso
+          img.setAttribute('src', 'idosa.png')
+          }
        res.style.textAlign = 'center'
        res.innerHTML = `Detectamos ${genero} com ${idade} anos!`
+       res.appendChild(img)
      } 
 }
