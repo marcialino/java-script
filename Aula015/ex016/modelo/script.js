f48142aa68550e777df5d06c69b4f5694f73cc71
marcialino <marcialino100@gmail.com>
2023-03-11 07:37:41 -0300

    criei o exe016 site contanto

--- Aula015/ex016/modelo/script.js
+++ Aula015/ex016/modelo/script.js
@@ -1,16 +1,35 @@
 function contar(){
     var n1 = window.document.getElementById('txtini')
     var n2 = window.document.getElementById('txtfim')
-    var pas = window.document.getElementById('txtpas')
-
-    n1 = Number(n1.value)
-    n2 = Number(n2.value)
-    pas = Number(pas.value)
-
+    var pas = window.document.getElementById('txtpas') 
+  
     var res = window.document.getElementById('res')
+   
+    if(n1.value.length == 0 || n2.value.length == 0 || pas.value.length == 0){
+        window.alert('[ERRO] Faltam dados!')
+        res.innerText = `Impossível contar`
+    }else{
+        n1 = Number(n1.value)
+        n2 = Number(n2.value)
+        pas = Number(pas.value)
 
-       for(var c = n1; c <= n2; c += pas){
-        res.innerHTML += `${c}`
-       }
-       
+        if(pas <= 0){
+            window.alert(`Passo inválido!`)
+        }
+
+        if(n1< n2){
+            //contagem crescente
+            for(var c = n1; c <= n2; c += pas){
+                res.innerHTML += ` ${c} \u{1F449}`
+               }
+        }else{
+            //contagem regressiva
+            for(var c = n1; c >= n2; c -= pas){
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+
+        }
+        
+       res.innerHTML += `\u{1F3C1}`
+    }
 }
