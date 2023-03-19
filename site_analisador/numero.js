b40b71eb76f1f3093ece24943a3c7bc600ef9faf
marcialino <marcialino100@gmail.com>
2023-03-19 20:19:18 -0300

    Criei site_analisador

---
+++ site_analisador/numero.js
@@ -0,0 +1,7 @@
+function clicar(){
+    var n = Number(window.document.getElementById('num'))
+
+    var area = window.document.getElementById('area')
+
+    area.innerHTML += `Valor ${n} foi adicionado`
+}
