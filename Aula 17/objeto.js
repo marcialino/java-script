c716200a6bfd894700a91639f868c75a291248e0
marcialino <marcialino100@gmail.com>
2023-03-21 15:30:17 -0300

    Criei Aula 17

---
+++ Aula 17/objeto.js
@@ -0,0 +1,14 @@
+/* Pode ser escrito desta maneira ou da forma a seguir:
+let amigo = {nome: 'José', sexo:'M', peso: 85.4,engordar(p){}, }
+console.log(amigo)*/
+
+let amigo = {nome: 'José',
+sexo: 'M',
+peso: 85.4,
+engordar (p=0){
+    console.log('Engordou')
+    this.peso += p
+}
+}
+amigo.engordar(2)
+console.log(`${amigo.nome} pesa ${amigo.peso} Kg.`)
