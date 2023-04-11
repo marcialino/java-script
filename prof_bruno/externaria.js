eb2106970be800b3f1e2450696693501b526e8d1
marcialino <marcialino100@gmail.com>
2023-04-11 14:21:41 -0300

    criei

---
+++ prof_bruno/externaria.js
@@ -0,0 +1,7 @@
+var num1 = 10
+var num2 = 100
+
+res =(num1 > num2 ? "VERDADEIRO" : "FALSO")
+console.log(res)
+
+// Teste lógico ? se verdadeiro : se falso
---
+++ prof_bruno/parimpar.js
@@ -0,0 +1,8 @@
+var num = 23
+var res = num%2
+
+if (res == 0){
+    console.log('PAR')
+}else{
+    console.log('IMPAR')
+}
